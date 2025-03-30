2f1cc8b3b5667f081a5d7a848fc3cb8254706292
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2025-03-30 16:08:15 -0400

    reafactor: refactor menssage in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h2>ProjetoX-VaiSerGerenciadorDeTasks</h2>
+    return <h2>ProjetoX - Vai Ser Um Gerenciador De Tasks E Centralizador de documentação</h2>
 }
 
 export default Home;
